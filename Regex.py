b606c815f1b948a8560cfc784c2519c35cb20fd1
Mieschendahl <mieschendahl.leonardo@gmail.com>
2021-04-14 16:47:35 +0200

    updated code

--- Regex.py
+++ Regex.py
@@ -26,7 +26,7 @@ class NFA:
             for state in current:
                 dct = self.transition.get(state, {})
                 for key in dct:
-                    if re.match(key, word[index], re.DOTALL):
+                    if re.match(key, word[index]):
                         newcurrent |= dct[key]
 
             current = newcurrent
@@ -54,7 +54,7 @@ class NFA:
             for state in current:
                 dct = self.transition.get(state, {})
                 for key in dct:
-                    if re.match(key, char, re.DOTALL):
+                    if re.match(key, char):
                         newcurrent |= dct[key]
 
             current = newcurrent
